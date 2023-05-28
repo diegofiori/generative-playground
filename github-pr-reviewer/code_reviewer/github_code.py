835c2650b640c7f46384062ea1599aa5b0ca7759
diegofiori <d.fiori@nebuly.ai>
2023-05-28 16:57:24 +0200

    Use AI based CodeReviewer

--- github-pr-reviewer/code_reviewer/github_code.py
+++ github-pr-reviewer/code_reviewer/github_code.py
@@ -1,10 +1,6 @@
-from github import Github
+from github import Github, Commit
 
-
-class CodeReviewer:
-    def __call__(self, code):
-        return [(1, 'This is a comment')]
-
+from code_reviewer.reviewer import CodeReviewer
 
 class GithubAPI:
     def __init__(self, access_token):
@@ -14,12 +10,13 @@ class GithubAPI:
     def write_comments_for_pr(self, repo_name, pr_number):
         repo = self.g.get_repo(repo_name)
         pr = repo.get_pull(pr_number)
+        commit = repo.get_commit(pr.head.sha)
         files = pr.get_files()
         for file in files:
             if file.status == 'added' or file.status == 'modified':
-                comments = self.code_reviewer(file.patch)
-                for position, comment in comments:
-                    pr.create_review_comment(body=comment, commit_id=pr.head.sha, path=file.filename, position=position)
+                comments = self.code_reviewer(file.filename, file.patch)
+                for filename, position, comment in comments:
+                    pr.create_review_comment(body=comment, commit_id=commit, path=filename, position=position)
         return 200
 
 
